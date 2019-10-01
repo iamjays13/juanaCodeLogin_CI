f63dcd885a8fc59e1978511210681e4263538c67
iamjays13 <35424926+iamjays13@users.noreply.github.com>
2019-10-01 16:02:27 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/login_juana_code/';
+$config['base_url'] = 'https://github.com/iamjays13/juanaCodeLogin_CI';
 
 /*
 |--------------------------------------------------------------------------
